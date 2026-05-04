dfe5289848f022841578a257302bb1ed675da464
winkwink-backend <rosariolettera62@gmail.com>
2026-05-04 11:45:06 +0200

    fix utils.js

--- utils.js
+++ utils.js
@@ -1,16 +1,5 @@
-import nodemailer from "nodemailer";
 import dotenv from 'dotenv';
 dotenv.config();
-
-export const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: false,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS
-  }
-});
 
 export const otpStore = new Map();
 
