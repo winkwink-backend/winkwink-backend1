b81d0d441b5d68ec1b735a0203082df8b6bc6ff4
winkwink-backend <rosariolettera62@gmail.com>
2026-05-10 16:56:15 +0200

    Aggiornato backend con nuove credenziali Railway

--- db.js
+++ db.js
@@ -4,7 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
   ssl: { rejectUnauthorized: false }
 });
 
